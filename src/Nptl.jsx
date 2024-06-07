cf79c967975387966605d2de11d492671bd03894
mohit <mohitsasane76@gmail.com>
2024-06-07 18:57:55 +0530

    added

---
+++ src/Nptl.jsx
@@ -0,0 +1,12 @@
+import img1 from "./assets/dbms certificate nptel.jpg"
+
+function Nptl(){
+    return(
+        <>
+            <div className="nptl">
+                <span><a href={img1} download="dbms certificate nptel.jpg">Course Certificate</a></span>
+            </div>
+        </>
+    );
+}
+export default Nptl;
